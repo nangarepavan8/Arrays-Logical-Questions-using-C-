82be641217e338e153c8c7789dd62286e2e1acbd
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-03 21:30:45 +0530

    2) Search Even number in arrays

--- Program.cs
+++ Program.cs
@@ -4,43 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _1_Search_array__Element
+namespace _2_Search_EvenNUmber_Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             int[] arr = new int[44];
-            int n, n1, i, flag = 0;
 
+            int n, n1, count =0;
             Console.WriteLine("Enter the size of array");
             n = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter the array element = ");
-
-            for( i=0; i<n; i++)
+            Console.WriteLine("Enter the element");
+            for (int i = 0; i < n; i++)
             {
-                arr[i] = int.Parse(Console.ReadLine());
+                arr[i] = int.Parse(Console.ReadLine()) ;
             }
-            Console.WriteLine("Enter element to search ");
-            n1 = int.Parse(Console.ReadLine());
-            for ( i=0; i<n; i++)
+
+            Console.WriteLine("The even numbers are = ");
+            for(int i =0; i<n; i++)
             {
-                if (arr[i] == n1 )
+                if (arr[i]%2 == 0)
                 {
-                    flag = 1 ;
-                    Console.WriteLine("Number "+ n1 +" Found at "+ (i+1) +" locations");
-                    break;
-                }    
-            }
-            if(flag == 0)
-            {
-                Console.WriteLine("Not found in array element "+ n1);
+                    Console.WriteLine(arr[i]);
+                }
             }
 
             Console.ReadLine();
-
-
         }
     }
 }

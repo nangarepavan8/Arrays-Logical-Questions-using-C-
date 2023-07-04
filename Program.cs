aafe4bcc2471f81b7216e91b6e7e1e22edf369a7
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-04 23:17:34 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,33 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _2_Search_EvenNUmber_Array
+namespace _3_FInd_Min_and_Second_Min_Number_Array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[44];
+            int min, secondMin;
+            int size, n, n2, count;
+            int[] arr =new int [50];
 
-            int n, n1, count =0;
-            Console.WriteLine("Enter the size of array");
-            n = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the element");
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Enter the size of any array");
+            size = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The "+ size +" elements are : ");
+           // n = int.Parse(Console.ReadLine());
+            for(int i =0; i<size; i++)
             {
-                arr[i] = int.Parse(Console.ReadLine()) ;
+               arr[i]=int.Parse(Console.ReadLine());
+               // Console.WriteLine(arr[i]);
             }
 
-            Console.WriteLine("The even numbers are = ");
-            for(int i =0; i<n; i++)
+            Console.WriteLine("The elements are : ");
+            for(int i =0; i<size;i++)
             {
-                if (arr[i]%2 == 0)
+                Console.WriteLine(arr[i]);
+            }
+            if (arr[0] < arr[1])
+            {
+                min = arr[0];
+                secondMin = arr[1];
+            }
+            else
+            {
+                min = arr[1];
+                secondMin = arr[0];
+            }
+
+            Console.WriteLine("The smallest elements is : ");
+            for (int i = 2; i < size; i++)
+            {
+                if (arr[i] < min)
                 {
-                    Console.WriteLine(arr[i]);
+                    secondMin = min;
+                    min = arr[i];
                 }
+                else if (arr[i]<secondMin)
+                {
+                    secondMin= arr[i];
+                }
+               
             }
+            Console.WriteLine("second min is : "+ secondMin);
+            Console.WriteLine("Min is : " + min);
 
             Console.ReadLine();
+
         }
     }
 }

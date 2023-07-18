a392f6bcc5253586d54bd3a9cc44e6cd25da345e
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-18 20:51:56 +0530

    10_reversestring with out inbuilt function

---
+++ 10_Reverese_String_Without_InbuiltFunction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Reverese_String_Without_InbuiltFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int len = 0;
+            string str, revStr = " ";
+
+            Console.WriteLine("enter the string");
+            str=Console.ReadLine();
+            
+            len  = str.Length-1;
+            while(len >= 0)
+            {
+                revStr  =revStr + str[len];
+                len--;
+            }
+
+            Console.WriteLine("The Reverse String are : " + revStr);
+
+            Console.ReadLine();
+        
+        }
+    }
+}

6458ae4fee46e3f3b9251994e12eb26158a6f826
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-26 22:51:43 +0530

    14_Sorting an array in ascending order

---
+++ 14_Sorting_Array_Ascending_order/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Sorting_Array_Ascending_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,temp, j, n;
+            int[] arr =  new int[50];
+
+            Console.WriteLine("Enter the size of an Array : ");
+            n=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter thes array elements are : ");
+            for(i=0;i<n;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine()); 
+            }
+
+            for(i=0;i<n;i++)
+            {
+                for(j=i+1;j<n;j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                       temp = arr[j];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("After ascending oder : ");
+            for(i=0;i<n; i++)
+            {
+                Console.WriteLine(arr[i] +"\t ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

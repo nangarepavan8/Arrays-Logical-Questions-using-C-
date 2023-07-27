64c8cdc50236e1d2910e7fd1b5a3ddde6221e315
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-27 22:03:43 +0530

    15_Selection sort by using arrays

---
+++ 15_Selection_Sort/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[50];
+            int i, j, x, y, n, temp;
+
+            Console.WriteLine("Enter the size of an array : ");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the elements are : ");
+
+            for(i=0; i < n;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(i=0; i<n-1;i++)
+            {
+                y= arr[i];
+                x = i;
+
+                for(j=i+1; j<n;j++)
+                {
+
+                    if (y > arr[j])
+                    {
+                        y = arr[j];
+                        x = j;
+                    }
+                }
+
+                temp = arr[i];
+                arr[i] = arr[x];
+                arr[x] = temp;
+            }
+
+
+            Console.WriteLine("Sorted elements arrray are : ");
+            for(i=0; i<n;i++)
+            {
+                Console.WriteLine(arr[i] + " \t");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}

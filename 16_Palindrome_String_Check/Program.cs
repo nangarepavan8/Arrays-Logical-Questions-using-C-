7fc97cefbde1e22f190775f803fb7445c8dd15b5
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-07-31 22:16:51 +0530

    16_Palindrome string check

---
+++ 16_Palindrome_String_Check/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Palindrome_String_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int i, j, flag = 1;
+
+            Console.WriteLine("Enter the string");
+            str = Console.ReadLine();
+
+            for (i = 0, j = str.Length - 1; j != 0; i++, j--)
+            {
+                if (str[i] == str[j])
+                {
+                    
+                }
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+
+            if(flag == 1) {
+                Console.WriteLine("The string is palindrom : "+ str);
+            }
+            else
+            {
+                Console.WriteLine("The string is Not Palindrome : "+ str);
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

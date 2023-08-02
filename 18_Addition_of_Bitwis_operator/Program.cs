134174bb931e76351600d7591bcb17783b1b2626
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2023-08-03 00:00:22 +0530

    18_Binary Addition of two numbers

---
+++ 18_Addition_of_Bitwis_operator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Addition_of_Bitwis_operator
+{
+    internal class Program
+    {
+        public static int add(int a, int b)
+        {
+            int hld;
+            while(b!=0)
+            {
+                hld = a & b;
+                a = a ^ b;
+                b = hld << 1;
+
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+
+            Console.WriteLine("Enter two Numbers : ");
+            a = int.Parse(Console.ReadLine());
+            b= int.Parse(Console.ReadLine());
+
+            sum = add(a, b);
+            Console.WriteLine("The Sum of number is : "+ sum);
+
+            Console.ReadLine();
+        }
+    }
+}
